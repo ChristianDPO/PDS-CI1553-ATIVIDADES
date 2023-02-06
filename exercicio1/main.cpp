9a8e6f3cd8671e765a93d88a61abf31111b643fa
christiandpo <christiandpo@gmail.com>
2023-02-05 21:14:54 -0300

    Mudado main do exercicio1 (aparentemente a função validarCPF não funciona)

--- exercicio1/main.cpp
+++ exercicio1/main.cpp
@@ -9,13 +9,22 @@ int main(){
 	std::cin >> p1.nome;
 	std::cout << "Idade: ";
 	std::cin >> idade;
+	
+	unsigned long cpf;
+
 	p1.idade = idade;
 	bool valido = false;
-	while(!valido){
-		std::cout << "CPF: ";
-		std::cin >> p1.cpf;
-		valido = p1.validarCPF(p1.cpf);
+	
+	std::cout << "CPF: ";
+	std::cin >> cpf;
+	valido = p1.validarCPF(cpf);
+	
+	if(!valido){
+		std::cout << "CPF invalido:" << cpf << std::endl;
+		return -1;
 	}
+
+	p1.cpf = cpf;
 	std::cout << "Dados da pessoa: " << p1.nome << "\t" << (unsigned short int)p1.idade << "\t" << p1.cpf << std::endl;
 
 	return 0;
